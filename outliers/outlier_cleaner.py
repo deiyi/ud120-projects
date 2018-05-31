09ab5ecc3e15467ba74b54f9dec460675001f171
david <david@DESKTOP-09SBUU6>
2018-05-31 14:01:47 +0200

    Regression mini-project, 2nd session

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -12,9 +12,24 @@ def outlierCleaner(predictions, ages, net_worths):
     """
     
     cleaned_data = []
-
+                                      
     ### your code goes here
-
+    variation = [abs(predictions[i][0] - net_worths[i][0]) for i in range(len(ages))]
+    touple = []  
     
+    for i in range(len(ages)):
+        touple.append([variation[i], ages[i][0], net_worths[i][0], predictions[i][0]])
+        # print variation[i], ages[i][0] , net_worths[i][0] , predictions[i][0]
+    
+    from operator import itemgetter
+    sortedTouple = sorted(touple, key = itemgetter(0))
+    for elem in sortedTouple:
+        #print elem
+        cleaned_data.append([elem[1] , elem[2] , elem[0]])
+    cleaned_data = cleaned_data[:-10]
+    
+    '''for elem in cleaned_data:
+        print elem'''
+        
     return cleaned_data
 
