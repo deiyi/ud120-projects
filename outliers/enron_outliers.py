d623017ea961063f498bed7e47f0670af45b149b
david <david@DESKTOP-09SBUU6>
2018-06-01 18:02:49 +0200

    Outliers mini-project last part

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -7,13 +7,40 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
 
+
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
-data = featureFormat(data_dict, features)
 
 
 ### your code below
+def findMaxSalaryName(data_dict):
+    maxSalary = 0
+    maxBonus = 0
+    maxName = ""
+    for name in data_dict.keys():
+        if data_dict[name]["salary"] != "NaN":
+            if data_dict[name]["salary"] > maxSalary:
+                maxName = name
+                maxSalary = data_dict[name]["salary"]
+                maxBonus = data_dict[name]["bonus"]
+    print maxName, data_dict[maxName]["salary"], data_dict[maxName]["bonus"], " is a typo."
+    
+    return maxName  
 
+data_dict.pop(findMaxSalaryName(data_dict) , 0)
+data = featureFormat(data_dict, features)
 
+for name in data_dict.keys():
+    if data_dict[name]["bonus"] != "NaN" and data_dict[name]["salary"] != "NaN":
+        if data_dict[name]["bonus"] > 5e6 and data_dict[name]["salary"] > 1e6:
+            print name, data_dict[name]["salary"], data_dict[name]["bonus"]  
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
