22795c804a1611bc08f76e083cbdff418b1a361f
david <david@DESKTOP-09SBUU6>
2018-07-07 21:03:58 +0200

    Validation mini project code

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -10,6 +10,8 @@
     After that, it's not our code anymore--it's yours!
 """
 
+# C:\ProgramData\Anaconda27\python.exe validate_poi.py
+
 import pickle
 import sys
 sys.path.append("../tools/")
@@ -28,5 +30,12 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels, test_size=0.3, random_state=42)
 
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print "Accuracy: " , clf.score(features_test, labels_test)
 
