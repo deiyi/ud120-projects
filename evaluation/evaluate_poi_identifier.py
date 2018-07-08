16d77b8589a30a2d1c12ffc5bcc2c99a03284ca9
david <david@DESKTOP-09SBUU6>
2018-07-08 11:32:01 +0200

    Evaluation mini project code

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -11,6 +11,8 @@
     Start by loading/formatting the data...
 """
 
+# C:\ProgramData\Anaconda27\python.exe evaluate_poi_identifier.py
+
 import pickle
 import sys
 sys.path.append("../tools/")
@@ -20,12 +22,41 @@ data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") 
 
 ### add more features to features_list!
 features_list = ["poi", "salary"]
-
+sort_keys = '../tools/python2_lesson14_keys.pkl'
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
 
 ### your code goes here 
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels, test_size=0.3, random_state=42)
 
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print "Accuracy: " , clf.score(features_test, labels_test)
 
+pred = clf.predict(features_test)
+print "Amount of predicted POIs: " , int(sum(pred))
+print "Amount of people: " , len(features_test)
+
+countTruePos = 0
+for i, feature in enumerate(features_test):
+    if feature == pred[i] and pred[i] == 1:
+        countTruePos = countTruePos + 1
+print "Count True Positive: " , countTruePos
+
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+print "Precission score: " , precision_score(labels_test, pred, average=None)
+print "Recall score: " , recall_score(labels_test, pred, average=None)
+
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1] 
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+print "Precission score: " , precision_score(true_labels, predictions, average=None)
+
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1] 
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+print "Recall score: " , recall_score(true_labels, predictions, average=None)
